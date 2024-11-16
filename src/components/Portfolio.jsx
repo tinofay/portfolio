55eba996fb04ff87a671ff3195c3cee85a5baa38
tinofay <tfzungunde@outlook.com>
2024-11-16 08:57:15 +0200

    update README.md

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import coming_soon from "../assets/portfolio/coming-soon.jpg"
-// import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-// import installNode from "../assets/portfolio/installNode.jpg";
-// import navbar from "../assets/portfolio/navbar.jpg";
-// import reactParallax from "../assets/portfolio/reactParallax.jpg";
-// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-// import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
   const portfolios = [
